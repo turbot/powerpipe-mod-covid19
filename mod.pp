fb4dd149807583ea185379ce980d589fed2842b4
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-04 20:26:06 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -2,5 +2,5 @@ mod "covid19_analysis_dashboard" {
   title         = "Covid19 Analysis Dashboard"
   description   = ""
   documentation = file("./README.md")
-  categories    = ["postgresql"]
+  categories    = ["dashboard", "postgresql"]
 }
