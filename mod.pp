57fb3c459175ba9f88f03e07a9ad5fe86b8bd98d
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-05 21:27:15 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -5,3 +5,9 @@ mod "covid19" {
   documentation = file("./README.md")
   categories    = ["dashboard", "postgresql"]
 }
+
+opengraph {
+    title       = "Powerpipe Mod for Covid19"
+    description = "Analyze COVID-19 data using PostgreSQL and Powerpipe."
+    image       = "/images/mods/turbot/covid19-social-graphic.png"
+}
