abe6d9300296c30e76682732f6a558b4598cb4ec
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-05 21:24:51 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -1,6 +1,7 @@
 mod "covid19" {
   title         = "Covid19"
   description   = "Analyze COVID-19 data using PostgreSQL and Powerpipe."
+  color         = "#72A945"
   documentation = file("./README.md")
   categories    = ["dashboard", "postgresql"]
 }
