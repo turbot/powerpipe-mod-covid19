ad2838460c557884d9c6b115d5ff1e292881c8c4
misraved <ved@turbot.com>
2024-03-06 21:37:40 +0530

    Update the social graphics and icon path for the mod

--- mod.pp
+++ mod.pp
@@ -3,13 +3,13 @@ mod "covid19" {
   description   = "Visualize COVID-19 vaccinations, healthcare capacity, regional trends, and more using Powerpipe and PostgreSQL."
   documentation = file("./docs/index.md")
   color         = "#72A945"
-  icon          = "/images/mods/turbot/covid19-dashboard.svg"
+  icon          = "/images/mods/turbot/covid19-insights.svg"
   documentation = file("./README.md")
   categories    = ["dashboard", "postgres"]
 
   opengraph {
     title       = "Powerpipe Mod for Covid19"
     description = "Visualize COVID-19 vaccinations, healthcare capacity, regional trends, and more using Powerpipe and PostgreSQL."
-    image       = "/images/mods/turbot/covid19-dashboard-social-graphic.png"
+    image       = "/images/mods/turbot/covid19-insights-social-graphic.png"
   }
 }
